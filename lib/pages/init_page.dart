078e997159d1dda80bfd4219a56c7dd11ce5a3c0
lacoca <032287d@utea.edu.pe>
2023-11-11 09:15:11 -0500

    TERMINANDO_CON_BOTONES

--- lib/pages/init_page.dart
+++ lib/pages/init_page.dart
@@ -8,21 +8,45 @@ class InitPage extends StatelessWidget {
     return Scaffold(
 
       bottomNavigationBar: BottomNavigationBar( 
+      type: BottomNavigationBarType.fixed,  ///nos permite aumentar bas de 3 botones      
         items: [ 
           BottomNavigationBarItem(
-            label: "home",
+            label: "PRINCIPAL",
             icon: Icon( 
-              Icons.add,
+              Icons.home_filled,
             ),
             ),
             BottomNavigationBarItem(
-              label: "perfil",
+              label: "SHORTS",
               icon: Icon(  
-                Icons.add,
-              )
-              )
+                Icons.play_arrow_rounded
+                              ),
+              ),
+            BottomNavigationBarItem(
+              label: "",
+              icon: Container(
+                margin: const EdgeInsets.only(top: 4.0), //la línea de código que proporcionaste establece un margen de 4.0 píxeles en la parte superior del widget al que se aplica
+                child:const Icon( 
+                 
+                    Icons.add_circle_outline,
+                    size: 36.0,
+                    ),
+                     ),
+                   ),
+                
+                BottomNavigationBarItem(
+                label: "SUSCRIPCIONES",
+                icon: Icon(  
+                 Icons.subscriptions_rounded,
+                ),
+               ),
+              BottomNavigationBarItem(
+                label: "BIBLIOTECA",
+                icon: Icon( 
+                  Icons.video_collection_rounded,
+                ),
+              ),
         ],
-
       ),
     ); ///es una estructura de alto nivel que proporciona la estructura visual básica para una aplicación Flutter
   }
