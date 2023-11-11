6854879214dd794b72ec04365fa21a95c0aa68ed
lacoca <032287d@utea.edu.pe>
2023-11-11 07:44:44 -0500

    COMMIT_2_InitPage

---
+++ lib/pages/init_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InitPage extends StatelessWidget {
+  const InitPage({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(); ///es una estructura de alto nivel que proporciona la estructura visual básica para una aplicación Flutter
+  }
+}
