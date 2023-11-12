740cafe77fe418fceb0b6609c585f6595305e44b
lacoca <032287d@utea.edu.pe>
2023-11-12 18:12:17 -0500

    trabajando con videos

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trabajo_final/ui/general/colors.dart';
 import 'package:trabajo_final/ui/widgets/item_filter_widget.dart';
+import 'package:video_player/video_player.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,6 +17,7 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             SingleChildScrollView(
+              
               scrollDirection: Axis.horizontal,
               physics: const BouncingScrollPhysics(),
               child: Row(
@@ -61,6 +63,18 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+
+            Column( 
+              children: [ 
+                Image.network(
+                  "https://vod-progressive.akamaized.net/exp=1699843582~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2199%2F19%2F485996873%2F2176892505.mp4~hmac=e57302d0d8cdc38b5f2a1005b45ad8ab1b4b4dccf487455b4f5cc1e2472b70be/vimeo-prod-skyfire-std-us/01/2199/19/485996873/2176892505.mp4",
+                ),
+              ListTile( 
+                title: Text("Ladislao Costillo Camacho"),
+                subtitle: Text(""),
+              ),
+              ],
             ),
           ],
         ),
