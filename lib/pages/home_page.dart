4a20a3dfeee920e4cd0d967b4473828a11c83b15
lacoca <032287d@utea.edu.pe>
2023-11-12 17:14:45 -0500

    BOTONES MIX,MUSICA, TODOS, PROGRAMACION

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trabajo_final/ui/general/colors.dart';
+import 'package:trabajo_final/ui/widgets/item_filter_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,37 +15,52 @@ class HomePage extends StatelessWidget {
         ),
         child: Column(
           children: [
-            Row(
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    primary: kBrandSecondaryColor,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 10.0,
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              child: Row(
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      primary: kBrandSecondaryColor,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                    ),
+                    icon: const Icon(
+                      Icons.explore_outlined,
+                    ),
+                    label: const Text(
+                      "Explorar", // Agrega el texto que desees aquí
                     ),
                   ),
-                  icon: const Icon(
-                    Icons.explore_outlined,
+                  const SizedBox(
+                    height: 32.0,
+                    child: VerticalDivider(
+                      color: kBrandSecondaryColor,
+                      thickness: 1.0,
+                    ),
                   ),
-                  label: const Text(
-                    "Explorar", // Agrega el texto que desees aquí
+                  ItemFilterWidget(
+                    text: "Todos",
+                    isSelected: true,
                   ),
-                ),
-               const SizedBox( 
-                  height: 32.0,
-                child: VerticalDivider(   //divide con una linea vertical
-                  color: kBrandSecondaryColor,
-                  thickness: 1.0,
-
-                 ),
-
-                ),
-                
-              Chip(label: Text("Todos"),),
-                
-              ],
+                  ItemFilterWidget(
+                    text: "Mixes",
+                    isSelected: false,
+                  ),
+                  ItemFilterWidget(
+                    text: "Musica",
+                    isSelected: false,
+                  ),
+                  ItemFilterWidget(
+                    text: "Programacion",
+                    isSelected: true,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
--- lib/ui/widgets/item_filter_widget.dart
+++ lib/ui/widgets/item_filter_widget.dart
@@ -1,23 +1,28 @@
-
 import 'package:flutter/material.dart';
 import 'package:trabajo_final/ui/general/colors.dart';
 
 class ItemFilterWidget extends StatelessWidget {
   String text;
+  bool isSelected; 
 
-ItemFilterWidget({required this.text,});
+  ItemFilterWidget({
+
+    required this.text,
+    required this.isSelected,
+    });
+
   @override
-  Widget build(BuildContext context){ 
+  Widget build(BuildContext context) {
     return Container(
-                  margin: EdgeInsets.only(right: 10.0),
-                  child: Chip(
-                    backgroundColor: kBrandSecondaryColor,
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                     ),
-                    label: Text("Todos"),
-                    ),
-                    );
+      margin: const EdgeInsets.only(right: 10.0),
+      child: Chip(
+        backgroundColor: isSelected ? Colors.white : kBrandSecondaryColor,
+        labelStyle: TextStyle(
+          color: isSelected ? kBrandPrimaryColor: Colors.white,
+        ),
+        label: Text(text), // Utiliza la variable 'text' aquí en lugar de "text"
+      ),
+    );
   }
 }
 
