9aa223dcf33036a9340c3e6fd8694c09393bdfb5
lacoca <032287d@utea.edu.pe>
2023-11-15 21:23:13 -0500

    insertando30 videos

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,14 +1,22 @@
 
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart%20';
+import 'package:trabajo_final/models/video_model.dart';
 class APIService { 
 
 getVideos() async {
-  String _path="https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAEKLE8aMsLIxl2GrNql7OXZ5sNVYV0Ad8&maxResults=20&regionCode=PE";
+  List<VideoModel> videoModel = [ ];
+  String _path="https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAEKLE8aMsLIxl2GrNql7OXZ5sNVYV0Ad8&maxResults=30&regionCode=PE";
 Uri _Uri= Uri.parse(_path);
 http.Response response = await http.get(_Uri);
-print(response.statusCode);
-print (response.body);
- }
+if (response.statusCode == 200){ 
+  Map<String, dynamic> MyApp = json.decode (response.body);
+  List videos =MyApp  [ "items"];
+  videoModel=videos.map((e) => VideoModel.fromJson(e)).toList();
+  print(videoModel);
 
+ }
+}
 }
